bec7e7d75f03556b806d08bd653a89edcdf36ff9
vmcarabbacan <vincent@buyanyinsurance.com>
2023-01-17 15:16:56 +0400

    add localhost origin

--- src/config/allowedOrigin.js
+++ src/config/allowedOrigin.js
@@ -5,6 +5,8 @@ const allowedOrigins = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://192.168.86.178:8080',
+    'http://127.0.0.1:8080/',
+    'http://192.168.86.178:8080/',
 ]
 
 module.exports = allowedOrigins
