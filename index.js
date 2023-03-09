07d2bd454db2df9c168edeb419e03c71781a3922
Vincent Mark Bondoc Carabbacan <vmbcarabbacan@gmail.com>
2023-03-09 08:06:53 +0400

    socket

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ const mongoose = require("mongoose");
 const { PORT } = process.env;
 // socket
 const { createServer } = require("http");
-// const { Server } = require("socket.io");
-const socket = require("socket.io");
+const { Server } = require("socket.io");
 const httpServer = createServer(app);
-// const socket = require('./src/services/socketio')
+const socket = require('./src/services/socketio')
 
+const io = new Server(httpServer, { cors: {
+  origin: 'http://localhost:8080',
+  methods: ["GET", "POST"],
+}});
 
 
 connectDB();
@@ -34,10 +37,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-// socket(io);
+socket(io);
 
-const loginUser = new Map();
-    
 app.use("/", express.static(path.join(__dirname, "public")));
 
 routes.map((x) => {
@@ -46,68 +47,14 @@ routes.map((x) => {
 
 app.use(errorHandler);
 
-// mongoose.connection.once("open", () => {
-//   // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   httpServer.listen(PORT);
-// });
-
-mongoose
-  .connect(process.env.DATABASE_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+mongoose.connection.once("open", () => {
+  // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  httpServer.listen(PORT);
+});
 
 mongoose.connection.on("error", (err) => {
   logEvents(
     `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
     "mongoErrLog.log"
   );
-});
-
-
-const server = app.listen(PORT, () =>
-  console.log(`Server started on ${PORT}`)
-);
-
-// const io = new Server(httpServer, { cors: {
-//   origin: 'http://localhost:8080',
-//   methods: ["GET", "POST"],
-// }});
-
-const io = socket(server, {
-  cors: {
-    origin: "http://localhost:8080",
-    credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-      console.log('user disconnected');
-  });
-
-  socket.on('send-data', (data) => {
-      socket.emit('receive-data', 'hello');
-  })
-
-  socket.on('login', (userId) => {
-      loginUser.set(userId, socket.id);
-      socket.emit('receive-data', loginUser);
-  })
-
-  socket.on('logout', (userId) => {
-      loginUser.delete(userId);
-  })
-
-  socket.on('updated-profile', (data) => {
-      socket.emit('receive-data', 'hellos');
-      // const notifId = loginUser.get(data._id);
-  })
 });
